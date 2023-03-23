182a2b1e4e29e61eebedef04c5db28bd59fd6a31
VanJelly <jamesmankhokwe@gsmil.com>
2023-03-23 11:40:21 -0700

    Is Upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: character to check
- *
- * Return: 0 or 1
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 if sucessful
  */
+
 int _isupper(int c)
-
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c < 91)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
-
+	}
 }
